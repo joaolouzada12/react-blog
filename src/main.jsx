f942ba845fb47e45a0aa42d82a9f187bae43b4ff
João Louzada <joaolouzada2017@gmail.com>
2025-07-24 20:49:03 -0300

    Set up initial React project structure with routing and basic layout

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { Children, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+import Home from './routes/Home.jsx'
+import Newpost from './routes/Newpost.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <Newpost />,
+      },
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
---
+++ src/routes/Newpost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Newpost = () => {
+  return (
+    <div>
+      Newpost
+    </div>
+  )
+}
+
+export default Newpost
